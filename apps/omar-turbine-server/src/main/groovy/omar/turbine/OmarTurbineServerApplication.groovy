2da2ddcbb336e34fe7902bf590f4c687b591e87a
Garrett Potts <gpotts@radiantblue.com>
2017-04-05 08:05:43 -0400

    Initial import of turbine

---
+++ apps/omar-turbine-server/src/main/groovy/omar/turbine/OmarTurbineServerApplication.groovy
@@ -0,0 +1,16 @@
+package omar.turbine
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.cloud.netflix.turbine.EnableTurbine
+
+@SpringBootApplication
+@EnableTurbine
+@EnableHystrixDashboard
+class OmarTurbineServerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run OmarTurbineServerApplication, args
+	}
+}
