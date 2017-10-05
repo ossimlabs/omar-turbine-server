cca3c85a6e576cf0ca477c903e87af9ca9084c29
Natalie <nroberts@radiantblue.com>
2017-10-05 17:50:49 -0400

    Testing turbine

--- apps/omar-turbine-server/src/main/groovy/omar/turbine/OmarTurbineServerApplication.groovy
+++ apps/omar-turbine-server/src/main/groovy/omar/turbine/OmarTurbineServerApplication.groovy
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.cloud.netflix.turbine.EnableTurbine
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableTurbine
 @EnableHystrixDashboard
+@EnableEurekaClient
+
 class OmarTurbineServerApplication {
 
 	static void main(String[] args) {
