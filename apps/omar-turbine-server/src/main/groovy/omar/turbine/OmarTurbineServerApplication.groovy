8a3af42cefb072e000aee222c9ba81d968db0cbc
Natalie <nroberts@radiantblue.com>
2017-10-12 14:57:48 -0400

    changed annotation to EnableDiscoveryClient from EnableEurekaClient

--- apps/omar-turbine-server/src/main/groovy/omar/turbine/OmarTurbineServerApplication.groovy
+++ apps/omar-turbine-server/src/main/groovy/omar/turbine/OmarTurbineServerApplication.groovy
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.cloud.netflix.turbine.EnableTurbine
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableTurbine
 @EnableHystrixDashboard
-@EnableEurekaClient
+@EnableDiscoveryClient
 
 class OmarTurbineServerApplication {
 
